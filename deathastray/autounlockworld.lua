304170e304710b29734b3228a2a5518ae3d6b67e
Nikaaaa <162158483+Diandrare@users.noreply.github.com>
2026-01-30 04:50:01 +0700

    unlock

---
+++ deathastray/autounlockworld.lua
@@ -0,0 +1,132 @@
+-- Auto Teleport Script Nikaa
+-- Total Locations: 16 Start unlock farm
+
+-- Step 1: Point 1
+print("Teleporting to Point 1...");
+local player = game.Players.LocalPlayer;
+if player and player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
+  player.Character.HumanoidRootPart.CFrame = CFrame.new(-1185.0312, 0.62983453, -164.10378);
+end
+task.wait(1);
+
+-- Step 2: Point 2
+print("Teleporting to Point 2...");
+local player = game.Players.LocalPlayer;
+if player and player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
+  player.Character.HumanoidRootPart.CFrame = CFrame.new(-1217.0647, 0.6458725, -129.18932);
+end
+task.wait(1);
+
+-- Step 3: Point 3
+print("Teleporting to Point 3...");
+local player = game.Players.LocalPlayer;
+if player and player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
+  player.Character.HumanoidRootPart.CFrame = CFrame.new(-1298.5419, 0.3497023, -74.90581);
+end
+task.wait(1);
+
+-- Step 4: Point 4
+print("Teleporting to Point 4...");
+local player = game.Players.LocalPlayer;
+if player and player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
+  player.Character.HumanoidRootPart.CFrame = CFrame.new(-1322.5864, 0.8785037, -72.85094);
+end
+task.wait(1);
+
+-- Step 5: Point 5
+print("Teleporting to Point 5...");
+local player = game.Players.LocalPlayer;
+if player and player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
+  player.Character.HumanoidRootPart.CFrame = CFrame.new(-1139.3246, 0.64355075, -177.90009);
+end
+task.wait(1);
+
+-- Step 6: Point 6
+print("Teleporting to Point 6...");
+local player = game.Players.LocalPlayer;
+if player and player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
+  player.Character.HumanoidRootPart.CFrame = CFrame.new(-1093.6312, 3.9864018, -118.805466);
+end
+task.wait(1);
+
+-- Step 7: Point 7
+print("Teleporting to Point 7...");
+local player = game.Players.LocalPlayer;
+if player and player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
+  player.Character.HumanoidRootPart.CFrame = CFrame.new(-1199.0286, 0.33705753, -176.40387);
+end
+task.wait(1);
+
+-- Step 8: Point 8
+print("Teleporting to Point 8...");
+local player = game.Players.LocalPlayer;
+if player and player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
+  player.Character.HumanoidRootPart.CFrame = CFrame.new(-1181.6497, 0.3380322, -154.1642);
+end
+task.wait(1);
+
+-- Step 9: Point 9
+print("Teleporting to Point 9...");
+local player = game.Players.LocalPlayer;
+if player and player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
+  player.Character.HumanoidRootPart.CFrame = CFrame.new(-1206.2471, 0.47268912, -213.61589);
+end
+task.wait(1);
+
+-- Step 10: Point 10
+print("Teleporting to Point 10...");
+local player = game.Players.LocalPlayer;
+if player and player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
+  player.Character.HumanoidRootPart.CFrame = CFrame.new(-1259.6237, 13.281165, -222.3405);
+end
+task.wait(1);
+
+-- Step 11: Point 11
+print("Teleporting to Point 11...");
+local player = game.Players.LocalPlayer;
+if player and player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
+  player.Character.HumanoidRootPart.CFrame = CFrame.new(-1153.1256, 0.54028213, -241.50594);
+end
+task.wait(1);
+
+-- Step 12: Point 12
+print("Teleporting to Point 12...");
+local player = game.Players.LocalPlayer;
+if player and player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
+  player.Character.HumanoidRootPart.CFrame = CFrame.new(-1326.1122, 0.86801326, -45.010036);
+end
+task.wait(1);
+
+-- Step 13: Point 13
+print("Teleporting to Point 13...");
+local player = game.Players.LocalPlayer;
+if player and player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
+  player.Character.HumanoidRootPart.CFrame = CFrame.new(-1399.6757, 13.281165, -246.82439);
+end
+task.wait(1);
+
+-- Step 14: Point 14
+print("Teleporting to Point 14...");
+local player = game.Players.LocalPlayer;
+if player and player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
+  player.Character.HumanoidRootPart.CFrame = CFrame.new(-1328.6968, 0.8573407, -15.465847);
+end
+task.wait(1);
+
+-- Step 15: Point 15
+print("Teleporting to Point 15...");
+local player = game.Players.LocalPlayer;
+if player and player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
+  player.Character.HumanoidRootPart.CFrame = CFrame.new(-1356.8251, 0.8628243, -66.48615);
+end
+task.wait(1);
+
+-- Step 16: Point 16
+print("Teleporting to Point 16...");
+local player = game.Players.LocalPlayer;
+if player and player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
+  player.Character.HumanoidRootPart.CFrame = CFrame.new(-1357.4528, 0.85162246, -32.93776);
+end
+task.wait(1);
+
+print("Sequence Complete.");
